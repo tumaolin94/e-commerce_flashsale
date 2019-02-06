9943bd31e80b1ad249f52943d1531b7a74cecf55
Maolin Tu <tumaolin94@gmail.com>
2019-02-05 22:57:31 -0800

    add order static page

--- src/main/java/com/maolintu/flashsale/controller/FlashSaleController.java
+++ src/main/java/com/maolintu/flashsale/controller/FlashSaleController.java
@@ -5,6 +5,7 @@ import com.maolintu.flashsale.domain.FlashsaleOrder;
 import com.maolintu.flashsale.domain.OrderInfo;
 import com.maolintu.flashsale.domain.SaleUser;
 import com.maolintu.flashsale.result.CodeMsg;
+import com.maolintu.flashsale.result.Result;
 import com.maolintu.flashsale.service.FlashSaleService;
 import com.maolintu.flashsale.service.GoodsService;
 import com.maolintu.flashsale.service.OrderService;
@@ -16,8 +17,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/sale")
@@ -41,35 +44,40 @@ public class FlashSaleController {
 
   private static Logger logger = LoggerFactory.getLogger(FlashSaleController.class);
 
-  @RequestMapping("/do_buy")
-  public String doBuy(Model model, SaleUser user, @RequestParam("goodsId") long goodsId){
+  @PostMapping("/do_buy")
+  @ResponseBody
+  public Result<OrderInfo> doBuy(Model model, SaleUser user, @RequestParam("goodsId") long goodsId){
 
     model.addAttribute("user", user);
     if(user == null){
-      return "login";
+//      return "login";
+      return Result.error(CodeMsg.SESSION_ERROR);
     }
 
     GoodsVo goods = goodsService.getGoodsVoByGoodsId(goodsId);
     int stock = goods.getStockCount();
     if(stock <= 0){
-      model.addAttribute("errmsg", CodeMsg.SALE_OVER.getMsg());
-      return "buy_fail";
+//      model.addAttribute("errmsg", CodeMsg.SALE_OVER.getMsg());
+//      return "buy_fail";
+      return Result.error(CodeMsg.SALE_OVER);
     }
 
     FlashsaleOrder order = orderService.getSaleOrderByUserIdGoodsId(user.getId(), goodsId);
 
     if(order != null){
-      model.addAttribute("errmsg", CodeMsg.REPEATE_ORDER.getMsg());
-      return "buy_fail";
+//      model.addAttribute("errmsg", CodeMsg.REPEATE_ORDER.getMsg());
+//      return "buy_fail";
+      return Result.error(CodeMsg.REPEATE_ORDER);
     }
 
     //decrease stock, make an order
     OrderInfo orderInfo = flashSaleService.completeOrder(user, goods);
 
-    model.addAttribute("orderInfo", orderInfo);
-    model.addAttribute("goods", goods);
+//    model.addAttribute("orderInfo", orderInfo);
+//    model.addAttribute("goods", goods);
 
-    return "order_detail2";
+//    return "order_detail2";
+    return Result.success(orderInfo);
   }
 
 }
