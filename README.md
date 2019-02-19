7bf5d342dfaeb22132e4e34fd6fc03afc750303a
Maolin Tu <tumaolin94@gmail.com>
2019-02-18 18:45:26 -0800

    finish

--- README.md
+++ README.md
@@ -1,11 +1,35 @@
 # e-commerce_flashsale
 
+### Requirement:
+1. Spring Boot
+2. MySQL
+3. Redis
+4. RabbitMQ
 
-Optimization result:
+### Entry
 
-Test by Jmeter with 5000 * 10 threads
+`http://hostname:8080/login/to_login`
 
-4 Cores, 16 GB Memory per server
+
+### Before Pressure Test
+```
+1. run main function of UserUtil to create multiple users into MySQL
+2. if has already create users, run generateTokens() to generate tokens.txt
+3. import tokens.txt into jmeter
+3. run: sh jmeter -n -t jmeter_script.jmx -l result.jtl
+```
+
+
+
+### Optimization result:
+
+Test by Jmeter with 5000 * 10 threads, 
+
+Test request: `Buy operation: POST request`
+
+
+
+4 Cores, 16 GB Memory per server, 1 backend server in total.
 
 1. original QPS: 1310
 
--- src/main/java/com/maolintu/flashsale/util/UserUtil.java
+++ src/main/java/com/maolintu/flashsale/util/UserUtil.java
@@ -60,48 +60,53 @@ public class UserUtil {
 		//insert database
 		insertDB(users);
 
-		String urlString = "http://localhost:8080/login/do_login";
-		File file = new File("tokens.txt");
-		if(file.exists()) {
-			file.delete();
-		}
-		RandomAccessFile raf = new RandomAccessFile(file, "rw");
-		file.createNewFile();
-		raf.seek(0);
-		for(int i=0;i<users.size();i++) {
-			SaleUser user = users.get(i);
-			URL url = new URL(urlString);
-			HttpURLConnection co = (HttpURLConnection)url.openConnection();
-			co.setRequestMethod("POST");
-			co.setDoOutput(true);
-			OutputStream out = co.getOutputStream();
-			String params = "mobile="+user.getId()+"&password="+MD5Util.inputPassFormPass("123456");
-			out.write(params.getBytes());
-			out.flush();
-			InputStream inputStream = co.getInputStream();
-			ByteArrayOutputStream bout = new ByteArrayOutputStream();
-			byte buff[] = new byte[1024];
-			int len = 0;
-			while((len = inputStream.read(buff)) >= 0) {
-				bout.write(buff, 0 ,len);
-			}
-			inputStream.close();
-			bout.close();
-			String response = new String(bout.toByteArray());
-			JSONObject jo = JSON.parseObject(response);
-			String token = jo.getString("data");
-			System.out.println("create token : " + user.getId());
-			
-			String row = user.getId()+","+token;
-			raf.seek(raf.length());
-			raf.write(row.getBytes());
-			raf.write("\r\n".getBytes());
-			System.out.println("write to file : " + user.getId());
-		}
-		raf.close();
-		
-		System.out.println("over");
+		//generate tokens
+    generateTokens(users);
 	}
+
+	public static void generateTokens(List<SaleUser> users) throws Exception{
+    String urlString = "http://localhost:8080/login/do_login";
+    File file = new File("tokens.txt");
+    if(file.exists()) {
+      file.delete();
+    }
+    RandomAccessFile raf = new RandomAccessFile(file, "rw");
+    file.createNewFile();
+    raf.seek(0);
+    for(int i=0;i<users.size();i++) {
+      SaleUser user = users.get(i);
+      URL url = new URL(urlString);
+      HttpURLConnection co = (HttpURLConnection)url.openConnection();
+      co.setRequestMethod("POST");
+      co.setDoOutput(true);
+      OutputStream out = co.getOutputStream();
+      String params = "mobile="+user.getId()+"&password="+MD5Util.inputPassFormPass("123456");
+      out.write(params.getBytes());
+      out.flush();
+      InputStream inputStream = co.getInputStream();
+      ByteArrayOutputStream bout = new ByteArrayOutputStream();
+      byte buff[] = new byte[1024];
+      int len = 0;
+      while((len = inputStream.read(buff)) >= 0) {
+        bout.write(buff, 0 ,len);
+      }
+      inputStream.close();
+      bout.close();
+      String response = new String(bout.toByteArray());
+      JSONObject jo = JSON.parseObject(response);
+      String token = jo.getString("data");
+      System.out.println("create token : " + user.getId());
+
+      String row = user.getId()+","+token;
+      raf.seek(raf.length());
+      raf.write(row.getBytes());
+      raf.write("\r\n".getBytes());
+      System.out.println("write to file : " + user.getId());
+    }
+    raf.close();
+
+    System.out.println("over");
+  }
 	
 	public static void main(String[] args)throws Exception {
 		createUser(5000);
